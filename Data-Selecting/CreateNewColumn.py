2eb622552e51b66e57fd3b517030512ba315572b
Chirag Soni <csoni0693@gmail.com>
2024-06-23 19:33:04 +0530

    pandas

---
+++ Data-Selecting/CreateNewColumn.py
@@ -0,0 +1,12 @@
+import pandas as pd
+# create new column
+data = {
+    'name': ['Piper', 'Grace', 'Georgia', 'Willow', 'Finn', 'Thomas'],
+    'salary': [4548, 28150, 1103, 6593, 74576, 24433]
+}
+df = pd.DataFrame(data)
+# print(df)
+def createBonusColumn(employees:pd.DataFrame)->pd.DataFrame:
+    employees['bonus'] = employees['salary'] * 2
+    return employees
+print(createBonusColumn(df))
---
+++ Data-Selecting/SelectData.py
@@ -0,0 +1,13 @@
+import pandas as pd
+# Select data
+data = {
+    'student_id': [101, 53, 128, 3],
+    'name': ['Ulysses', 'William', 'Henry', 'Henry'],
+    'age': [13, 10, 6, 11]
+}
+df = pd.DataFrame(data)
+# print(df)
+def selectData(students:pd.DataFrame)->pd.DataFrame:
+    return students.loc[students['student_id']==101,['name','age']]
+print(selectData(df))
+    
